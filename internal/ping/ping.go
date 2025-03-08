98b994e3fb24b133d8d971c3ef803df9cc15573a
Andre Doruk <81973147+AndreDoruk@users.noreply.github.com>
2025-03-08 23:14:36 +0100

    losing it 2

--- internal/ping/ping.go
+++ internal/ping/ping.go
@@ -1,44 +1,47 @@
 package schedule
 
 import (
-	"time"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/AndreDoruk/robo-baby-0.5/internal/database"
+	"github.com/bwmarrin/discordgo"
 )
 
-type validFunc func()
+func main() {
 
-func Loop(name string, duration time.Duration, function validFunc) {
-	sleepTime := getSleepTime(name, duration)
-
-	time.AfterFunc(sleepTime, func() {
-		function()
-		updateSleepTime(name)
-		Loop(name, duration, function)
-	})
-}
-
-func getSleepTime(name string, duration time.Duration) time.Duration {
-	times := make(map[string]time.Time)
-
-	database.LoadJson("db/timed.json", &times)
-	defer database.SaveJson("db/timed.json", times)
-
-	lastTime, exists := times[name]
-
-	if !exists {
-		times[name] = time.Now()
-		return 0 * time.Hour
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + os.Getenv("TOKEN"))
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
 	}
 
-	return time.Until(lastTime.Add(duration))
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
 }
 
-func updateSleepTime(name string) {
-	times := make(map[string]time.Time)
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	database.LoadJson("db/timed.json", &times)
-	defer database.SaveJson("db/timed.json", times)
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
 
-	times[name] = time.Now()
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
 }
