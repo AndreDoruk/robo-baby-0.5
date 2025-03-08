b743a9ef3aa90273e25a31f06b244c0d4b833b90
Andre Doruk <81973147+AndreDoruk@users.noreply.github.com>
2025-03-08 23:12:50 +0100

    losing it

---
+++ internal/ping/ping.go
@@ -0,0 +1,44 @@
+package schedule
+
+import (
+	"time"
+
+	"github.com/AndreDoruk/robo-baby-0.5/internal/database"
+)
+
+type validFunc func()
+
+func Loop(name string, duration time.Duration, function validFunc) {
+	sleepTime := getSleepTime(name, duration)
+
+	time.AfterFunc(sleepTime, func() {
+		function()
+		updateSleepTime(name)
+		Loop(name, duration, function)
+	})
+}
+
+func getSleepTime(name string, duration time.Duration) time.Duration {
+	times := make(map[string]time.Time)
+
+	database.LoadJson("db/timed.json", &times)
+	defer database.SaveJson("db/timed.json", times)
+
+	lastTime, exists := times[name]
+
+	if !exists {
+		times[name] = time.Now()
+		return 0 * time.Hour
+	}
+
+	return time.Until(lastTime.Add(duration))
+}
+
+func updateSleepTime(name string) {
+	times := make(map[string]time.Time)
+
+	database.LoadJson("db/timed.json", &times)
+	defer database.SaveJson("db/timed.json", times)
+
+	times[name] = time.Now()
+}
